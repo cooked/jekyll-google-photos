bc51e861a82714c03719f345181c86997c0304dc
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2022-08-23 23:04:04 +0200

    fix warning

--- lib/jekyll-google-photos/tag.rb
+++ lib/jekyll-google-photos/tag.rb
@@ -13,7 +13,7 @@ module JekyllGooglePhotos
     end
 
     def getImageLinks(url)
-      doc = Nokogiri::HTML(URI.open(url.strip).read)
+      doc = Nokogiri::HTML(URI.open(url.strip))
       scripts = doc.xpath("//script")
       jsonString = ""
       for x in scripts do
